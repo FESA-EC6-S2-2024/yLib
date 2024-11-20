71e27cf982607ca712590dd041a20b57199cf9c1
Luiz Eduardo Bartolassi <luizekbartolassi@gmail.com>
2024-11-19 23:47:53 -0300

    Format SecurityConfig and add login success handler

--- src/main/java/br/edu/fesa/yLib/security/SecurityConfig.java
+++ src/main/java/br/edu/fesa/yLib/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import java.util.List;
 
@@ -23,57 +24,59 @@ import java.util.List;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/", "/home", "/auth/register", "/css/**", "/images/**").permitAll()
-                        .requestMatchers("/loans/**").hasAnyRole(UserType.ADMIN.name(), UserType.LIBRARIAN.name())
-                        .requestMatchers("/loans/client").hasRole(UserType.CLIENT.name())
-                        .anyRequest().authenticated())
-                .formLogin((form) -> form
-                        .loginPage("/auth/login")
-                        .defaultSuccessUrl("/home", true)
-                        .failureUrl("/auth/login?error=true")
-                        .permitAll())
-                .logout((logout) -> logout
-                        .logoutUrl("/auth/logout")
-                        .logoutSuccessUrl("/auth/login")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll())
-                .sessionManagement((session) -> session
-                        .maximumSessions(1) // Ensures only one session per user is active
-                        .expiredUrl("/auth/login")) // Redirect to login if session expires
-                .exceptionHandling((exc) -> exc
-                        .accessDeniedPage("/error/forbidden")); // 403 error page
-        return http.build();
-    }
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((authorize) -> authorize
+            .requestMatchers("/", "/home", "/auth/register", "/css/**", "/images/**").permitAll()
+            .requestMatchers("/loans/**").hasAnyRole(UserType.ADMIN.name(), UserType.LIBRARIAN.name())
+            .requestMatchers("/loans/client").hasRole(UserType.CLIENT.name())
+            .anyRequest().authenticated())
+        .formLogin((form) -> form
+            .loginPage("/auth/login")
+            .successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
+            // .defaultSuccessUrl("/home", true)
+            .failureUrl("/auth/login?error=true")
+            .permitAll())
+        .logout((logout) -> logout
+            .logoutUrl("/auth/logout")
+            .logoutSuccessUrl("/auth/login")
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+            .permitAll())
+        .sessionManagement((session) -> session
+            .maximumSessions(1) // Ensures only one session per user is active
+            .expiredUrl("/auth/login")) // Redirect to login if session expires
+        .exceptionHandling((exc) -> exc
+            .accessDeniedPage("/error/forbidden")); // 403 error page
+    return http.build();
+  }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return username -> {
-            User user = userRepository
-                    .findByEmail(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return username -> {
+      User user = userRepository
+          .findByEmail(username)
+          .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-            return new CustomUserDetails(
-                    user.getEmail(),
-                    user.getPassword(),
-                    user.getName(),
-                    user.getImage(),
-                    true,
-                    user.isUsing2FA(),
-                    List.of(new SimpleGrantedAuthority("ROLE_" + user.getUserType().name())) // usando enum UserType para definir role
-            );
-        };
-    }
+      return new CustomUserDetails(
+          user.getEmail(),
+          user.getPassword(),
+          user.getName(),
+          user.getImage(),
+          true,
+          user.isUsing2FA(),
+          List.of(new SimpleGrantedAuthority("ROLE_" + user.getUserType().name())) // usando enum UserType para definir
+                                                                                   // role
+      );
+    };
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
